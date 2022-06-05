1be4b20f510995f1c8dff6038d7c79a26f5e9672
JailsonTS <jailson.santos@foton.la>
2022-06-05 08:22:53 -0400

    pastas e dependências iniciais

---
+++ app/src/main/java/com/picpay/desafio/dto/User.kt
@@ -0,0 +1,43 @@
+package com.picpay.desafio.dto
+
+import android.os.Parcel
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+
+
+data class User(
+    @SerializedName("id") var id: Int?,
+    @SerializedName("img") var img: String?,
+    @SerializedName("name") var name: String?,
+    @SerializedName("username") var username: String?,
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeValue(id)
+        parcel.writeString(img)
+        parcel.writeString(name)
+        parcel.writeString(username)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<User> {
+        override fun createFromParcel(parcel: Parcel): User {
+            return User(parcel)
+        }
+
+        override fun newArray(size: Int): Array<User?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/picpay/desafio/enums/enums.kt
@@ -0,0 +1,8 @@
+package com.picpay.desafio.enums
+
+ enum class enums(val url: String) {
+    PICPAY_CONTATOS("https://609a908e0f5a13001721b74e.mockapi.io/picpay/api/")
+}
+
+
+
